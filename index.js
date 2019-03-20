43800498704fe6c8c0ff2e2a80113cf023303f79
Martin Haynes <martin.haynes@creative-assembly.com>
2019-03-20 14:16:14 +0000

    corrects XO errors

--- index.js
+++ index.js
@@ -1,135 +1,126 @@
 module.exports = class DynoMight {
+	constructor(db, tableName, definition = {}) {
+		this.db = db;
+		this.tableName = tableName;
+		this.definition = definition;
+	}
 
-    constructor(db, tableName, definition = {}) {
-        this.db = db;
-        this.tableName = tableName;
-        this.definition = definition;
-    }
+	get(key) {
+		return new Promise((resolve, reject) => {
+			this.db.get({
+				TableName: this.tableName,
+				Key: {
+					[this._keyField()]: key
+				}
+			}, (err, result) => {
+				if (err) {
+					reject(err);
+				} else if (result.item) {
+					resolve(this._mapFields(result.item));
+				} else {
+					resolve({});
+				}
+			});
+		});
+	}
 
-    get(key) {
-        return new Promise((resolve, reject) => {
-            this.db.get({
-                TableName: this.tableName,
-                Key: {
-                    [this._keyField()] : key
-                }
-            }, (err, result) => {
-                if(err) {
-                    reject(err);
-                } else {
-                    if (result.item) {
-                        resolve(this._mapFields(result.item));
-                    } else {
-                        reject({});
-                    }
-                }
-            });
-        });
-    }
+	put(key, payload) {
+		return new Promise((resolve, reject) => {
+			const Item = {...{
+				[this._keyField()]: key
+			}, ...payload};
 
-    put(key, payload) {
-        return new Promise((resolve, reject) => {
+			this.db.put({
+				TableName: this.tableName,
+				Item
+			}, err => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(Item);
+				}
+			});
+		});
+	}
 
-            const Item = {...{
-                [this._keyField()]: key
-            }, ...payload};
+	isValid(payload) {
+		const validation = [];
 
-            this.db.put({
-                TableName: this.tableName,
-                Item
-            }, (err) => {
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(Item);
-                }
-            });
-        });
-    }
+		this._definitionAsArray().forEach(entities => {
+			const [field, data] = entities;
 
-    isValid(payload) {
+			if (field in payload) {
+				const item = payload[field];
+				if (this._isType(data, 'object')) {
 
-        const validation = [];
+				} else if (data === true) {
+					validation.push(this._test(this._isRequired(item), `${field} is required`));
+				}
+			}
+		});
+	}
 
-        this._definitionAsArray().forEach((entities) => {
-            const [field, data] = entities;
+	_test(result, message) {
+		return result === false ? message : result;
+	}
 
-            if ( field in payload ) {
-                const item = payload[field];
-                if(this._isType(data, 'object')) {
-                    
-                } else {
-                    if (data === true) {
-                        validation.push(this._test(this._isRequired(item), `${field} is required`));
-                    }
-                }
-            }
-        })
-    }
+	_hasKey(payload) {
+		return Object.keys(payload).lastIndexOf(this._keyField()) !== -1;
+	}
 
-    _test(result, message) {
-        return !result ? message : true;
-    }
+	_isType(data, type) {
+		return type(data) === type;
+	}
 
-    _hasKey(payload) {
-        return Object.key(payload).lastIndexOf(this._keyField()) !== -1;
-    }
+	_isRequired(data) {
+		let valid = true;
+		switch (typeof (data)) {
+			case 'number':
+				valid = data !== 0;
+				break;
+			case 'object':
+				if (Array.isArray) {
+					valid = data.length > 0;
+				} else {
+					valid = Object.keys(data).length > 0;
+				}
 
-    _isType(data, type) {
-        return type(data) === type;
-    }
+				break;
+			case 'symbol':
+			case 'string':
+				valid = data.length;
+				break;
+			case 'boolean':
+				valid = data === true || data === false;
+				break;
+			case 'function':
+			case 'undefined':
+			default:
+				valid = false;
+				break;
+		}
 
-    _isRequired(data) {
-        let valid = true;
-        switch(typeof(data)) {
-            case "number" :
-                valid = data !== 0
-                break;
-            case "object" :
-                if(Array.isArray) {
-                    vaild = data.length > 0;
-                } else {
-                    valid = Object.keys(data).length > 0
-                }
-                break;
-            case "symbol":
-            case "string" :
-                valid = data.length;
-                break;
-            case "boolean" :
-                valid = data === true || data === false;
-                break;
-            case "function" :
-            case "undefined" :
-            default:
-                valid = false;
-                break;
-        }
-        return valid;
-    }
+		return valid;
+	}
 
-    _keyField() {
-        return this._definitionAsArray().find((entities) => {
-            const [field, data] = entities;
-            if(data.isKey === true) {
-                return field;
-            }
-        }) || null;
-    }
+	_keyField() {
+		return this._definitionAsArray().find(entities => {
+			return (entities[1].isKey === true);
+		}) || null;
+	}
 
-    _mapFields(item) {
-        let responseData = {};
-        this._definitionAsArray().forEach((entities) => {
-            const [field, data] = entities;
-            if(field in item) {
-                responseData[field] = data;
-            }
-        });
-        return responseData;
-    }
+	_mapFields(item) {
+		const responseData = {};
+		this._definitionAsArray().forEach(entities => {
+			const [field, data] = entities;
+			if (field in item) {
+				responseData[field] = data;
+			}
+		});
+		return responseData;
+	}
 
-    _definitionAsArray() {
-        return Object.entries(this.definition);
-    }
-
-}
+	_definitionAsArray() {
+		return Object.entries(this.definition);
+	}
+};

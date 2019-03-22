1f40dba3489c42d62e4cf1e32d27e60c594c6524
Martin Haynes <martin.haynes@creative-assembly.com>
2019-03-22 12:51:55 +0000

    removes dynamodb-localhost

---
+++ specs/index.spec.js
@@ -0,0 +1,11 @@
+const {expect} = require('chai');
+
+const Dynomight = require('..');
+
+describe("Dyn-O-Might", () => {
+
+    describe('#construct', function() {
+        it("should create a new instance of the class");
+    })
+
+})
